c3122f6e6b98d1121b394bd57f139c8849b8b5e4
Malik Elgabroun <malik.elgabroun@redboxdigital.com>
2017-04-02 21:41:38 +0100

    Shareboard

--- controllers/shares.php
+++ controllers/shares.php
@@ -4,4 +4,12 @@ class Shares extends Controller{
 		$viewmodel = new ShareModel();
 		$this->returnView($viewmodel->Index(), true);
 	}
+
+	protected function add(){
+	    if(!isset($_SESSION['is_logged_in'])){
+	        header('Location:'.ROOT_URL.'shares');
+        }
+		$viewmodel = new ShareModel();
+		$this->returnView($viewmodel->add(), true);
+	}
 }
--- views/main.php
+++ views/main.php
@@ -1,8 +1,8 @@
 <html>
 <head>
 	<title>Shareboard</title>
-	<link rel="stylesheet" href="assets/css/bootstrap.css">
-	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="stylesheet" href="<?php echo ROOT_PATH; ?>assets/css/bootstrap.css">
+	<link rel="stylesheet" href="<?php echo ROOT_PATH; ?>assets/css/style.css">
 </head>
 <body>
 	<nav class="navbar navbar-default">
@@ -14,18 +14,25 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Shareboard</a>
+          <a class="navbar-brand">Shareboard</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
             <li><a href="<?php echo ROOT_URL; ?>">Home</a></li>
             <li><a href="<?php echo ROOT_URL; ?>shares">Shares</a></li>
           </ul>
+            <?php if($_SESSION['is_logged_in']) : ?>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="<?php echo ROOT_URL; ?>shares">Welcome <?php echo $_SESSION['user_data']['name']; ?></a></li>
+                    <li><a href="<?php echo ROOT_URL; ?>users/logout">Logout</a></li>
+                </ul>
+            <?php else: ?>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="<?php echo ROOT_URL; ?>users/login">Login</a></li>
+                    <li><a href="<?php echo ROOT_URL; ?>users/register">Register</a></li>
+                </ul>
+            <?php endif; ?>
 
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="<?php echo ROOT_URL; ?>users/login">Login</a></li>
-            <li><a href="<?php echo ROOT_URL; ?>users/register">Register</a></li>
-          </ul>
         </div><!--/.nav-collapse -->
       </div>
     </nav>
@@ -33,6 +40,7 @@
     <div class="container">
 
      <div class="row">
+         <?php Messages::displayMsg(); ?>
      	<?php require($view); ?>
      </div>
 
